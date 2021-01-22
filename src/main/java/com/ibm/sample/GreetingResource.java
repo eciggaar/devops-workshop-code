8a9ed0efddf15162d1d6bd85234068bfe11ca27f
Edward Ciggaar <edward_ciggaar@nl.ibm.com>
2021-01-22 15:54:09 +0100

    Changed code to consume config property

--- src/main/java/com/ibm/sample/GreetingResource.java
+++ src/main/java/com/ibm/sample/GreetingResource.java
@@ -5,12 +5,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/hello")
 public class GreetingResource {
+    @ConfigProperty(name = "greeting.message") String msg;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy v2 from the DevOps workshop!!";
+        return msg;
     }
 }
